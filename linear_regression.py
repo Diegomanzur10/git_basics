e73cc123718b0c3c83703cd038ea8483cdde9bd3
diego <diego.manzur@eia.edu.co>
2021-08-28 19:54:59 -0500

    To prove this new way of add commits

--- linear_regression.py
+++ linear_regression.py
@@ -7,6 +7,7 @@ X = np.array([[1,1], [1,2], [2,2], [2,3]])
 # y = 1 * x_0 + 2 * x_1 + 3
 y = np.dot(X, np.array([1,2])) + 3
 
+## Another comment to add
 reg = LinearRegression().fit(X, y)
 
 reg.score(X,y)
