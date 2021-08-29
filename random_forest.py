9870f1203389c4ae34d045273dc1f7221b444501
diego <diego.manzur@eia.edu.co>
2021-08-28 20:46:51 -0500

    Is this going to be uploaded to GitHub?

---
+++ random_forest.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import pathlib as Path
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.io as pio
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.cluster import FeatureAgglomeration
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.feature_selection import SelectPercentile, f_regression
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import MinMaxScaler, RobustScaler
+# from tpot.builtins import StackingEstimator, ZeroCount
+
+# exported_pipeline = make_pipeline(
+#     RandomForestRegressor(bootstrap=True, max_features=0.7500000000000001, min_samples_leaf=5, min_samples_split=9, n_estimators=100)
+# )
