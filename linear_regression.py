768f37e0c0d44c847701d68114899ac8280402e8
diego <diego.manzur@eia.edu.co>
2021-08-28 19:43:26 -0500

    Just add a comment.

--- linear_regression.py
+++ linear_regression.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-
+## Comment to check
 X = np.array([[1,1], [1,2], [2,2], [2,3]])
 # y = 1 * x_0 + 2 * x_1 + 3
 y = np.dot(X, np.array([1,2])) + 3
